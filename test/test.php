b1728887df6c2570f34eba0da101b1b9bde32f0c
Daniel Madureira <daniel.madureira.batista@gmail.com>
2017-02-07 14:44:17 -0200

    Add commenteds on test

--- test/test.php
+++ test/test.php
@@ -1,7 +1,31 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use OL\Arr;
+use OL\Arr\Arr;
 
+/**
+ * Create a object
+ */
 $array = new Arr([1,2,3]);
 print_r($array);
+
+
+/**
+ * Immutable object
+ */
+$array->map(function ($value) {
+  return $value + 1;
+});
+print_r($array);
+
+/**
+ * New object returned
+ */
+$array2 = $array->map(function ($value) {
+  return $value + 1;
+});
+print_r($array2);
+
+print_r(array_map(function($value) {
+  return $value + 1;
+}, [1,2,3]));
